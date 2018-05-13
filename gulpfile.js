a5a7c2b6e702c1eb3fb8abc4d3a61a5efd8ff34f
serjyudin <magnettoboy@gmail.com>
2018-05-13 21:31:29 +0300

    Таб

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,16 @@
 var syntax        = 'scss'; // Syntax: sass or scss;
 
 var gulp          = require('gulp'),
-	gutil         = require('gulp-util' ),
-	sass          = require('gulp-sass'),
-	browserSync   = require('browser-sync'),
-	concat        = require('gulp-concat'),
-	uglify        = require('gulp-uglify'),
-	cleancss      = require('gulp-clean-css'),
-	rename        = require('gulp-rename'),
-	autoprefixer  = require('gulp-autoprefixer'),
-	notify        = require("gulp-notify"),
-	rsync         = require('gulp-rsync');
+		gutil         = require('gulp-util' ),
+		sass          = require('gulp-sass'),
+		browserSync   = require('browser-sync'),
+		concat        = require('gulp-concat'),
+		uglify        = require('gulp-uglify'),
+		cleancss      = require('gulp-clean-css'),
+		rename        = require('gulp-rename'),
+		autoprefixer  = require('gulp-autoprefixer'),
+		notify        = require("gulp-notify"),
+		rsync         = require('gulp-rsync');
 
 gulp.task('browser-sync', function() {
 	browserSync({
