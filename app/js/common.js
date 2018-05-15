25ed324157710a159b62463356a13a3824f5ab82
serjyudin <magnettoboy@gmail.com>
2018-05-15 13:20:01 +0300

    Добавлена функция мульти-слайдера

--- app/js/common.js
+++ app/js/common.js
@@ -1,5 +1,12 @@
-$(function() {
-
 	// Custom JS
-
+$(document).ready(function(){
+  $('.single-slide').slick({
+    dots: true,
+  });
+  $('.multiple-slide').slick({
+    infinite: true,
+    slidesToShow: 2,
+    slidesToScroll: 1,
+    // dots: true,
+  });
 });
