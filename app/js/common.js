5e1eb15c3e91e29ada4b7055c76b42ac19db1169
serjyudin <magnettoboy@gmail.com>
2018-06-04 15:20:31 +0300

    Commit

--- app/js/common.js
+++ app/js/common.js
@@ -14,7 +14,7 @@ $(document).ready(function(){
 $(document).ready(function () {
   var navMain = document.querySelector('.main-nav');
   var navToggle = document.querySelector('.main-nav__toggle');
-  
+
   navToggle.addEventListener('click', function() {
     if (navMain.classList.contains('main-nav--closed')) {
       navMain.classList.remove('main-nav--closed');
