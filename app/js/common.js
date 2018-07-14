1e5606dbe8fdabc08e5afb63c28ec04b84c4ddca
serjyudin <magnettoboy@gmail.com>
2018-07-14 16:55:26 +0300

    Переход со слайдера SlickSlider на bxSlider (проблема responsive chrome)

--- app/js/common.js
+++ app/js/common.js
@@ -1,38 +1,7 @@
-	// Custom JS
-$(document).ready(function(){
-  $('.single-slide').slick({
-    dots: true,
-    responsive: [{
+//	Custom JS
 
-        breakpoint: 768,
-        settings: {
-          dots: false,
-          arrows: false,
-        }
-      }]
-  });
-  $('.multiple-slide').slick({
-    infinite: true,
-    slidesToShow: 2,
-    slidesToScroll: 1,
-    responsive: [{
-
-        breakpoint: 768,
-        settings: {
-          slidesToShow: 1,
-          dots: false,
-          arrows: false,
-        }
-      }]
-  });
-  $('.lolka').slick({
-    slidesToShow: 1,
-    slidesToScroll: 1,
-
-  });
-});
-
-$(document).ready(function () {
+// Кнопка вызова меню (переключатель)
+$(document).ready(function() {
   var navMain = document.querySelector('.main-nav');
   var navToggle = document.querySelector('.main-nav__toggle');
 
@@ -46,3 +15,170 @@ $(document).ready(function () {
     }
   });
 });
+
+
+// Управление слайдерами
+filling = $('.single-filling').bxSlider();
+multi = $('.multiple-slide').bxSlider();
+review = $('.single-review').bxSlider();
+
+function bxslider() {
+  var width = window.innerWidth;
+  if (width > 1220) {
+    filling.reloadSlider({
+      slideWidth: 750,
+      moveSlides: 1,
+      touchEnabled: true,
+    });
+    multi.reloadSlider({
+      randomStart: true,
+      minSlides: 2,
+      maxSlides: 2,
+      moveSlides: 1,
+      slideWidth: 320,
+      touchEnabled: true,
+    });
+    review.reloadSlider({
+      slideWidth: 750,
+      moveSlides: 1,
+      touchEnabled: true,
+    });
+  } else if (768 < width < 1220) {
+    filling.reloadSlider({
+      slideWidth: 470,
+      controls: false,
+      moveSlides: 1,
+      touchEnabled: true,
+    });
+    multi.reloadSlider({
+      // controls: false,
+      randomStart: true,
+      minSlides: 2,
+      maxSlides: 2,
+      moveSlides: 1,
+      slideWidth: 195,
+      touchEnabled: true,
+    });
+    review.reloadSlider({
+      // controls: false,
+      slideWidth: 470,
+      moveSlides: 1,
+      touchEnabled: true,
+    });
+  } else if (width < 768) {
+    filling.reloadSlider({
+      controls: false,
+      touchEnabled: true,
+    });
+    multi.reloadSlider({
+      controls: false,
+      randomStart: true,
+      // minSlides: 1,
+      // maxSlides: 1,
+      // moveSlides: 1,
+      slideWidth: 195,
+      touchEnabled: true,
+    });
+    review.reloadSlider({
+      controls: false,
+      touchEnabled: true,
+    });
+  }
+}
+bxslider();
+var resizeId;
+$(window).resize(function() {
+  clearTimeout(resizeId);
+  resizeId = setTimeout(doneResizing, 300);
+});
+
+function doneResizing() {
+  clearTimeout(resizeId);
+  bxslider();
+}
+
+// --------------------------------------------------------------------------------------
+
+// var slider1 = $('.single-slide').bxSlider();
+// var slider2 = $('.multiple-slide').bxSlider();
+// var slider3 = $('.-slide').bxSlider();
+//
+// var widthMatch = matchMedia("all and (max-width: 767px)");
+// var widthHandler = function(matchList) {
+//     if (matchList.matches) {
+//         slider2.reloadSlider({
+//           controls: false,
+//         });
+//     } else {
+//         slider2.reloadSlider({
+//           randomStart: true,
+//           minSlides: 2,
+//           maxSlides: 2,
+//           moveSlides: 1,
+//           slideWidth: 320,
+//         });
+//     }
+// };
+//
+// widthMatch.addListener(widthHandler);
+// widthHandler(widthMatch);
+
+
+// $(document).ready(function(){
+//   $('.single-slide').slick({
+//     dots: true,
+//     // responsive: [{
+//     //
+//     //     breakpoint: 768,
+//     //     settings: {
+//     //       dots: false,
+//     //       arrows: false,
+//     //     }
+//     //   }]
+//   });
+//   $('.multiple-slide').slick({
+//     infinite: true,
+//     slidesToShow: 2,
+//     slidesToScroll: 1,
+//     responsive: [{
+//
+//         breakpoint: 768,
+//         settings: {
+//           slidesToShow: 1,
+//           dots: false,
+//           arrows: false,
+//         }
+//       }]
+//   });
+//   $('.lolka').slick({
+//     slidesToShow: 1,
+//     slidesToScroll: 1,
+//
+//   });
+// });
+
+// $(window).on("load resize", function(){
+//   var width = $(document).width();
+//
+//   if (width <= 768) {
+//     $('.single-slide').slick({
+//       dots: false,
+//       arrows: false,
+//     });
+//     $('.multiple-slide').slick({
+//       slidesToShow: 1,
+//       dots: false,
+//       arrows: false,
+//     });
+//   } else {
+//     $('.single-slide').slick({
+//       dots: true,
+//       arrows: true,
+//     });
+//     $('.multiple-slide').slick({
+//       infinite: true,
+//       slidesToShow: 2,
+//       slidesToScroll: 1,
+//     });
+//   }
+// });
--- gulpfile.js
+++ gulpfile.js
@@ -37,7 +37,7 @@ gulp.task('styles', function() {
 gulp.task('js', function() {
 	return gulp.src([
 		'app/libs/jquery/dist/jquery.min.js',
-		'app/libs/slick/slick.min.js',
+		'app/libs/bxslider/jquery.bxslider.min.js',
 		'app/js/common.js', // Always at the end
 		])
 	.pipe(concat('scripts.min.js'))
